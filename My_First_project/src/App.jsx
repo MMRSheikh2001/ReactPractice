7863ec7acdf65cb1baeca7bb408ffb1e08850a82
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-08 17:46:02 +0600

    add

--- My_First_project/src/App.jsx
+++ My_First_project/src/App.jsx
@@ -5,24 +5,34 @@ import Card from './components/Card';
 import Counter from './components/Counter';
 
 
-const members = [
+// const members = [
+//     { name: "Mahbub", age: "23", address: "Dhaka" },
+//     { name: "Emon", age: "13", address: "Cumilla" },
+//     { name: "sabbir", age: "19", address: "Natore" },
+
+// ];
+
+function App() {
+
+    const [name, setName] = useState("");
+    const [age, setAge] = useState(0);
+    const [address, setAddress] = useState("");
+
+    const [members, setMembers] = useState([
     { name: "Mahbub", age: "23", address: "Dhaka" },
     { name: "Emon", age: "13", address: "Cumilla" },
     { name: "sabbir", age: "19", address: "Natore" },
 
-];
-
-function App() {
-
-    const [name,setName]=useState("");
+]);
 
 
     const handleSubmit = (e) => {
-      
+
         e.preventDefault();
-        console.log(name);
-
-
+        members.push({ name, age, address });
+        setMembers(members);
+        console.log(members);
+        
 
     }
 
@@ -30,9 +40,16 @@ function App() {
     return (
 
         <div>
-            <form onSubmit={(e)=>handleSubmit(e)}>
+            <form onSubmit={(e) => handleSubmit(e)}>
 
-                <input type="text" onChange={(e)=>setName(e.target.value)}/><br />
+                <label htmlFor="">Name</label>
+                <input type="text" onChange={(e) => setName(e.target.value)} /><br />
+
+                <label htmlFor="">Age</label>
+                <input type="number" onChange={(e) => setAge(e.target.value)} /><br />
+
+                <label htmlFor="">Address</label>
+                <input type="text" onChange={(e) => setAddress(e.target.value)} /><br />
                 <button type='submit'>Submit</button>
 
             </form>
